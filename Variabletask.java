7bf755b679958e2394c1e0b4a3f33e9ad23df4c6
Logeshwaran2020 <143994523+Logeshwaran2020@users.noreply.github.com>
2024-12-16 17:02:05 +0530

    Day 1 task 2

---
+++ Variabletask.java
@@ -0,0 +1,22 @@
+public class Variabletask{
+public static void main(String args[]){
+byte be = 007;
+System.out.println("byte : "+be);
+short s = 3227;
+System.out.println("short : "+s);
+int fav = 70929383;
+System.out.println("int : "+fav);
+long lg = 89455564892454l;
+System.out.println("long : "+lg);
+float ft = 528.43f;
+System.out.println("float : "+ft);
+double du = 25.487564d;
+System.out.println("double : "+du); 
+boolean bl = true;
+System.out.println("boolean : "+bl); 
+char letter = 'A';
+System.out.println("char : "+letter);
+String name = "Logshwaran G";
+System.out.println("name : "+name);
+}
+}
