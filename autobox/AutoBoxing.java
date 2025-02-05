373568944efbaf9202a4771719ecf2c36459949c
Logeshwaran2020 <143994523+Logeshwaran2020@users.noreply.github.com>
2025-02-05 16:13:53 +0530

    Autobox

---
+++ autobox/AutoBoxing.java
@@ -0,0 +1,54 @@
+package autobox;
+
+public class AutoBoxing {
+	public static void main(String[] args) {
+		//Int autoboxing and unboxing
+		int num = 27;
+		Integer num1 = num;
+		int num2 = num1;
+		System.out.println(num2);
+		
+		
+		//Long autoboxing and unboxing
+		long lg = 59876564;
+		Long lg1 = lg;
+		long lg2 = lg1;
+		System.out.println(lg);
+		
+		//Byte autoboxing and unboxing
+		byte by = 25;
+		Byte by1 = by;
+		byte by2 = by1;
+		
+		
+		//Boolean autoboxing and unboxing
+		boolean bl = true;
+		Boolean bl1=bl;
+		boolean bl2 = bl;
+		
+		
+		//Character autoboxing and unboxing
+		char ch = 's';
+		Character ch1 = ch;
+		char ch2 = ch1;
+		
+		
+		//Double autoboxing and unboxing
+		double db = 256458.4554;
+		Double db1 = db;
+		double db2 = db;
+		
+		//Float autoboxing and unboxing
+		float fl = 120.52f;
+		Float fl1 = fl;
+		float fl2 = fl1;
+		
+		//Short autoboxing and unboxing
+		short sh = 25;
+		Short sh1 = sh;
+		short sh2 = sh;
+		
+		
+	}
+
+}
